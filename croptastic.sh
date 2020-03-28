af1220bb0d8829bb4e23dfe3b7b726d0fb2baf34
BabyZion <sevcen23@gmail.com>
2020-03-28 17:06:35 +0200

    Logic for croping online and local streams added.

--- croptastic.sh
+++ croptastic.sh
@@ -22,9 +22,16 @@ scale=500
 output="test.mp4"
 
 # Video - works
-ffmpeg -ss 03:02 -i "${urls[0]}" -ss 03:02 -i "${urls[1]}" -map 0:v -map 1:a -ss 30 -t 00:20 -c:v libx264 -c:a aac ${output} -y
+# ffmpeg -ss 00:02 -i "${urls[0]}" -ss 00:02 -i "${urls[1]}" -map 0:v -map 1:a -ss 30 -t 01:00 -c:v libx264 -c:a aac test1.mp4 -y
+# Without skipping from the start.
+# ffmpeg -ss 00:00 -i "${urls[0]}" -ss 00:00 -i "${urls[1]}" -map 0:v -map 1:a -t 00:05 -c:v libx264 -c:a aac test5.mp4 -y
+if [ "$online_link" = true ]; then
+    ffmpeg -ss 00:00 -i "${urls[0]}" -ss 00:00 -i "${urls[1]}" -map 0:v -map 1:a -t 00:05 -c:v libx264 -c:a aac test5.mp4 -y
+else
+    ffmpeg -ss 00:00 -i "${urls[0]}" -ss 00:00 -i "${urls[0]}" -map 0:v -map 1:a -t 00:05 -c:v libx264 -c:a aac test5.mp4 -y
+fi
 
 # Gif -works
-ffmpeg -ss 03:17 -t 00:16 -i "${urls[0]}" -vf "fps=${fps},scale=${scale}-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 ${output} -y
+ffmpeg -ss 00:00 -t 00:05 -i "${urls[0]}" -vf "fps=${fps},scale=${scale}:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 gif_test.gif -y
 
 
